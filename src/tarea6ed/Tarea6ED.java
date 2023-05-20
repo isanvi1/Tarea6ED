fc496c84044e2dde29399457f390bc88c301acbe
IgnacioSanchez <sanchez.vilches@gmail.com>
2023-05-20 20:33:30 +0200

    Repositorio de prueba

--- src/tarea6ed/Tarea6ED.java
+++ src/tarea6ed/Tarea6ED.java
@@ -1,16 +1,13 @@
 package tarea6ed;
 
 /**
- * Clase principal para ejecutar el programa de descuento en productos.
- * 
+ *
  * @author Isanvi1
  */
 public class Tarea6ED {
 
     /**
-     * Método principal que crea una instancia de Tarea6EDClase y llama al método aplicarDescuento.
-     * 
-     * @param args los argumentos de la línea de comandos
+     * @param args the command line arguments
      */
     public static void main(String[] args) {
            
